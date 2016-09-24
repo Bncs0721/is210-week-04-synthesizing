7b1e02c692bc7ab793d59301daa5cf77e8ca6d4a
Biencis Rubiera <biencis@gmail.com>
2016-09-24 09:56:40 +0000

    Modified task_01.py to add celsius_to_kelvin function

--- task_01.py
+++ task_01.py
@@ -4,6 +4,8 @@
 
 import decimal
 
+ABSOLUTE_DIFFERENCE = decimal.Decimal('273.15')
+
 def fahrenheit_to_celsius(degrees):
     """Converts fahrenheit degrees to celsius.
 
@@ -11,17 +13,36 @@ def fahrenheit_to_celsius(degrees):
         degrees(int, float):Degrees in F to be converted to C.
 
     Returns:
-        Decimal:Degrees in C.
+        Decimal:Degrees in Celsius.
 
     Example:
 
         >>> fahrenheit_to_celsius(212)
         Decimal('100')
         
-    >>> fahrenheit_to_celsius(104.5)
-    Decimal('40.27777777777777777777777778')
+        >>> fahrenheit_to_celsius(104.5)
+        Decimal('40.27777777777777777777777778')
     """
     answer = ((decimal.Decimal(degrees - 32)) * 5)/9
     return answer
 
-    print answer
+def celsius_to_kelvin(degrees):
+    """Converts celsius to kelvin.
+
+    Arg:
+        degrees(int, float):Degrees in C to be converted to K.
+
+    Retunrs:
+        Decimal:Degrees in Kelvin.
+
+    Example:
+
+        >>> celsius_to_kelvin(100)
+        Decimal('373.15')
+        
+        >>> celsius_to_kelvin(150)
+        Decimal('423.15')
+
+    """
+    answer = ABSOLUTE_DIFFERENCE + degrees
+    return answer
